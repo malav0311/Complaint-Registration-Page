6f80b461293995ad1187e230b9994c846a228bf4
malav <malav1234@gmail.com>
2021-06-23 09:50:11 +0530

    final version

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pyrebase
 import json
+import pandas as pd
 
 def test_index(app,client):
       
@@ -25,3 +26,66 @@ def test_index(app,client):
 
 
 
+def test_route1(app, client):
+        del app
+        res = client.get('/')   
+  
+        assert res.status_code == 200
+
+
+def test_route2(app, client):
+        del app
+        res = client.get('/Complaint')   
+  
+        assert res.status_code == 200        
+
+def test_route3(app, client):
+        del app
+        res = client.get('/Dashboard1')   
+  
+        assert res.status_code == 200           
+
+def test_route4(app, client):
+        del app
+        res = client.get('/index')   
+  
+        assert res.status_code == 200   
+
+def test_route5(app, client):
+        del app
+        res = client.get('/status')   
+  
+        assert res.status_code == 200  
+
+def test_route6(app, client):
+        del app
+        res = client.get('/trackinfo')   
+  
+        assert res.status_code == 200 
+
+
+
+def not_null(app,client):
+      
+        firebaseConfig = {
+        "apiKey": "AIzaSyDV_CPfNZ_9D_gmQrxZcbo7SMS_9GQGYcE",
+        "authDomain": "city-5dc6f.firebaseapp.com",
+        "databaseURL": "https://city-5dc6f-default-rtdb.firebaseio.com",
+        "projectId": "city-5dc6f",
+        "storageBucket": "city-5dc6f.appspot.com",
+        "messagingSenderId": "876826563048",
+        "appId": "1:876826563048:web:d82f14c0aad5094cf6145d"
+        }
+        firebase = pyrebase.initialize_app(firebaseConfig)
+
+        db = firebase.database()
+        users = db.child("UserInfo").get()
+
+        l = []
+        for user in users.each():
+             l.append(user.val())
+        
+        df = pd.DataFrame(l)    
+
+        if(df.isnull().values.any() == False):
+             assert res.status_code == 200
